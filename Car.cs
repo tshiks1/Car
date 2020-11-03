b9ba5b11031c9f9ddb653ba3c5cb84bbf290c75a
Teresia Ngigi <tshiks22@outlook.com>
2020-11-03 11:54:47 -0600

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    public class Car
+    { 
+
+        /*In the Car class,
+    Create a Make property of type string that is public
+Create a Model property of type string that is public
+Create a Year property of type int that is public
+In the Main method,
+Create a new instance of the Car class
+Set values in the properties for the object
+Print the values of each property to the Console */
+
+    public string Make{ get; set;}
+    public int Year { get; set;}
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var honda = new Car();
+            honda.Make = "Honda";
+            honda.Year = 2012;
+            Console.WriteLine($"{honda.Make} is a good brand.\n  I have a {honda.Year } model.");
+
+        }
+    }
+}
