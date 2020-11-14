cc3eae271c192c58177a7fa35cb2c4d23879e087
Teresia Ngigi <tshiks22@outlook.com>
2020-11-13 19:24:32 -0600

    git intro

--- Car.cs
+++ Car.cs
@@ -5,8 +5,16 @@ using System.Text;
 namespace Car
 {
     public class Car
-    { 
+    {
+        public Car()
+            {
+            // default constructor.
+             }
 
+        public Car (string name)
+        {
+            name = "Lexus";
+        }
         /*In the Car class,
     Create a Make property of type string that is public
 Create a Model property of type string that is public
@@ -18,6 +26,7 @@ Print the values of each property to the Console */
 
     public string Make{ get; set;}
     public int Year { get; set;}
+    public string Model { get; set; }
 
 
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Car
 {
@@ -6,10 +7,39 @@ namespace Car
     {
         static void Main(string[] args)
         {
-            var honda = new Car();
+            //Dot Notation.
+            Car  honda = new Car();
             honda.Make = "Honda";
             honda.Year = 2012;
-            Console.WriteLine($"{honda.Make} is a good brand.\n  I have a {honda.Year } model.");
+            honda.Model = "HRV";
+
+            // Object initializer -Syntax Sugar.-Inline
+            var mazda = new Car() { Make = "Mazda", Model = "Protege", Year = 2012 };
+
+
+            //Extended syntax
+            var nissan = new Car()
+            {
+                Make = "Nissan",
+                Model = "Altima",
+                Year = 2014
+            };
+
+            Car lexus = new Car()
+            {
+                Make = "Toyota",
+                Model = "Corolla",
+                Year = 2020
+            };
+            List<Car> cars = new List<Car>() { honda, lexus, mazda, nissan };
+           
+            foreach(var item in cars)
+            { Console.WriteLine("-----");
+                Console.WriteLine($"In my list of cars I have :{item.Make} {item.Model}which was made in {item.Year } .");
+                
+            }
+
+           
 
         }
     }
